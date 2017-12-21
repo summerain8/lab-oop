ba4371e374cb683fd2cf87c986e9683b83d5fb5d
summerain8 <34695562+summerain8@users.noreply.github.com>
2017-12-21 21:48:57 +0300

    Add files via upload

---
+++ lab8/rhomb.cpp
@@ -0,0 +1,71 @@
+#include "rhomb.h"
+#include <cmath>
+#define PI 3.1415
+
+Rhomb::Rhomb()
+{
+	m_sideA = 0.0;
+	small_ang = 0.0;
+}
+
+Rhomb::Rhomb(std::istream& is)
+{
+	is >> *this;
+}
+
+void Rhomb::print() const
+{
+	std::cout << *this;
+}
+
+double Rhomb::area() const
+{
+	return m_sideA * small_ang;
+}
+
+Rhomb& Rhomb::operator = (const Rhomb& other)
+{
+	if (&other == this)
+		return *this;
+
+	m_sideA = other.m_sideA;
+	small_ang = other.small_ang;
+
+	return *this;
+}
+
+bool Rhomb::operator == (const Rhomb& other) const
+{
+	return m_sideA == other.m_sideA && small_ang == other.small_ang;
+}
+
+void* Rhomb::operator new (unsigned int size)
+{
+	return Figure::allocator.allocate();
+}
+
+void Rhomb::operator delete (void* p)
+{
+	Figure::allocator.deallocate(p);
+}
+
+std::ostream& operator << (std::ostream& os, const Rhomb& rectangle)
+{
+	os << "================" << std::endl;
+	os << "Figure type: rhomb" << std::endl;
+	os << "Side A size: " << rectangle.m_sideA << std::endl;
+	os << "Smaller angle: " << rectangle.small_ang << std::endl;
+
+	return os;
+}
+
+std::istream& operator >> (std::istream& is, Rhomb& rectangle)
+{
+	std::cout << "================" << std::endl;
+	std::cout << "Enter side A: ";
+	is >> rectangle.m_sideA;
+	std::cout << "Enter smaller angle: ";
+	is >> rectangle.small_ang;
+
+	return is;
+}
