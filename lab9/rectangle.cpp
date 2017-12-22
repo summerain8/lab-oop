9aa50566672ca78abe823c16045effaabfdc9c8c
summerain8 <34695562+summerain8@users.noreply.github.com>
2017-12-22 19:12:07 +0300

    Add files via upload

---
+++ lab9/rectangle.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <cmath>
+#include "rectangle.h"
+
+Rectangle::Rectangle() : Rectangle(0, 0)
+{
+}
+
+Rectangle::Rectangle(int32_t a, int32_t b) : side_a(a), side_b(b)
+{
+	
+}
+
+Rectangle::Rectangle(std::istream &is)
+{
+	std::cout << "Enter side a: ";
+	is >> side_a;
+	std::cout << "Enter side b: ";
+	is >> side_b;
+	
+	if (side_a < 0 || side_b < 0) {
+		std::cerr << "Error: side_as should be > 0." << std::endl;
+	}
+	
+}
+
+Rectangle::Rectangle(const Rectangle& orig)
+{
+	side_a = orig.side_a;
+	side_b = orig.side_b;
+}
+
+double Rectangle::Square()
+{
+	return (double)(side_a * side_b);
+}
+
+void Rectangle::Print()
+{
+	std::cout << "Side a = " << side_a << ", side b = " << side_b << ", square = " << this->Square() << ", type: rectangle" << std::endl;
+}
+
+Rectangle::~Rectangle()
+{
+}
+
+std::ostream& operator <<(std::ostream &os, const Rectangle &obj)
+{
+	os << "(" << obj.side_a << " " << obj.side_b << ")," << "type: rectangle" << std::endl;
+	return os;
+}
+
+std::istream& operator >>(std::istream &is, Rectangle &obj)
+{
+	std::cout << "Enter side_a: ";
+	is >> obj.side_a;
+	std::cout << "Enter smaller angle: ";
+	is >> obj.side_b;
+	return is;
+}
+
+bool Rectangle::operator ==(const Rectangle &obj) const
+{
+	return side_b == obj.side_b && side_a == obj.side_a;
+}
+
+
+Rectangle& Rectangle::operator =(const Rectangle &obj)
+{
+	if (&obj == this) {
+		return *this;
+	}
+
+	side_b = obj.side_b;
+	side_a = obj.side_a;
+
+	return *this;
+}
